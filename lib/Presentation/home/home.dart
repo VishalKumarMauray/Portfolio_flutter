b3fc401fb446af02b6ea81ccba1ea59618ad2e71
vishal kumar mauray <87877265+VishalKumarMauray@users.noreply.github.com>
2025-04-18 17:06:48 +0530

    header module

--- lib/Presentation/home/home.dart
+++ lib/Presentation/home/home.dart
@@ -1,28 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/Presentation/header/header.dart';
+import 'package:portfolio/utils/assets_constant.dart';
 import 'package:provider/provider.dart';
 
 import '../../theme/controller/theme_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double imageBOpacity = 1;
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Theme Provider Demo'),
-      //   actions: [
-      //     Switch(
-      //       value: themeProvider.isDarkMode,
-      //       onChanged: (value) {
-      //         themeProvider.toggleTheme(value);
-      //       },
-      //     ),
-      //   ],
-      // ),
-      body: const Center(
-        child: Text('Toggle the switch to change theme'),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 50),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    Assets.portfolioIcon,
+                    height: 150,
+                    width: 150,
+                  ),
+                  Stack(
+                    children: [
+                      AnimatedOpacity(
+                        opacity: imageBOpacity,
+                        duration: const Duration(milliseconds: 300),
+                        child: Image.asset(
+                          Assets.sun,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Positioned(
+                        top: 12,
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() async {
+                              if (imageBOpacity == 1) {
+                                imageBOpacity = 0;
+                                await Future.delayed(
+                                    const Duration(milliseconds: 100));
+                                themeProvider.toggleTheme(true);
+                              } else {
+                                imageBOpacity = 1;
+                                await Future.delayed(
+                                    const Duration(milliseconds: 100));
+                                themeProvider.toggleTheme(false);
+                              }
+                            });
+                          },
+                          child: Image.asset(
+                            Assets.cloud,
+                            height: 100,
+                            width: 120,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const Header(),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/utils/assets_constant.dart
+++ lib/utils/assets_constant.dart
@@ -1,3 +1,15 @@
 class Assets {
-  static const splash = "";
+  static const _base = "assets";
+
+  // header
+  static const portfolioIcon = "$_base/icon.png";
+  static const user = "$_base/user.png";
+  static const laptopBg = "$_base/laptop_bg.png";
+  static const github = "$_base/github.png";
+  static const linkedin = "$_base/linkedin.png";
+  static const telegram = "$_base/telegram.png";
+  static const whatsapp = "$_base/whatsapp.png";
+  static const email = "$_base/email.png";
+  static const sun = "$_base/image 18.png";
+  static const cloud = "$_base/image 19.png";
 }
